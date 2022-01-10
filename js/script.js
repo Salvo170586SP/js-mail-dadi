a6fcc3d5addfaa97f0aec677c2c0a95f5f40db11
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-01-10 15:36:13 +0100

    added if userMail

--- js/script.js
+++ js/script.js
@@ -15,6 +15,8 @@ Stampare in console i tiri di dado e il risultato.
 */
 
 
+//////////////////////////////////////////////////////////////////////////////
+
 //MAIL
 /* 
 -creo array di mail
@@ -28,18 +30,40 @@ Stampare in console i tiri di dado e il risultato.
 */
 
 
+
+
+//creo variabile per agganciare elemento in pagina
+const resultElement = document.getElementById('welcome');
+
 //creo array di mail
-const mails = ["salvo@gmail.com", "marco@gmail.com", "paolo@gmail.com"];
+const mails = ["salvo@gmail.com","marco@gmail.com","luca@gmail.com"];
 console.table(mails);
-
+ 
 //chiedo email all'utente
-const userMail = prompt("digita la tua mail", "salvo@gmail.com").trim();
-console.log(userMail);
-
-
+const userMail = prompt("digita la tua mail", "paolo@gmail.com").trim();
 
 
 //-ciclo la variabile mails per quante sono le mail nell'array
 for(let i = 0; i < mails.length; i++){
-    console.log(mails[i]); 
+    const currentElement = mails[i];
+    console.log(currentElement);
+
+
+
+    //creo condizione se la mail dell'utente è nella lista
+    let message = ``;
+
+    //se la mail è uguale alla mail della lista
+    if(userMail === mails[i]){
+        //stampa in pagina "sei registrato"
+        message += `${userMail} sei registrato`;
+        resultElement.innerText = message;
+
+    }else{
+        //altrimenti stampa in pagina "non sei registrato"
+        message += `${userMail} non sei registrato`;
+        resultElement.innerText = message;
+    }
 }
+
+
