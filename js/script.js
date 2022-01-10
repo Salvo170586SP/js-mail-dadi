55a08959072cd2a8283b55f9f03ac358f25305dc
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-01-10 14:25:01 +0100

    added first analyses

--- js/script.js
+++ js/script.js
@@ -1,2 +1,29 @@
 console.log('JS OK');
 
+/* 
+// # Mail
+Crea un array di email;
+Chiedi all’utente la sua email.
+controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato in console sull’esito del controllo.
+
+
+// # Gioco dei dadi
+ Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Stampare in console i tiri di dado e il risultato.
+
+*/
+
+
+//MAIL
+/* 
+-creo array di mail
+-chiedo email all'utente
+-creo condizione 
+    -se la email è nella lista
+        -stampo messaggio in console "sei autorizzato"
+    -altrimenti
+        -stampo messaggio in console "non sei registrato" 
+*/
+
+
